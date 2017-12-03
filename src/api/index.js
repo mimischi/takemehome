be7056f4595c36f1ba92d42e2218febcbe4aceee
Michael Gecht <gecht.m@gmail.com>
2017-12-03 14:59:15 +0100

    Set API_BASE_URL depending on environment

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.API_URL
+    : 'http:proxify.dev'
+
 const API = axios.create({
   timeout: 1000,
-  baseURL: 'http://proxify.dev'
+  baseURL: API_BASE_URL
 })
 
 export default API
