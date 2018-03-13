6dac306bc0566afcdf75e641ccbbe5204f99d733
Michael Gecht <gecht.m@gmail.com>
2018-03-13 20:14:11 +0100

    Update to new hotel hostname

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://proxify.test"'
+  API_URL: '"http://proxify.localhost"'
 })
