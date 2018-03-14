fdbd9dce2308d55ebd273e21bbbf96e4bb610c94
Michael Gecht <gecht.m@gmail.com>
2018-03-14 09:29:21 +0100

    Update layout breakpoints

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
   v-app(light)
-    v-toolbar(color="blue" dark fixed app)
-      v-toolbar-title(v-text="title")
-      //- v-spacer
-      //- v-toolbar-items
-        v-btn(flat to="/") HOME
-        v-btn(flat to="/departures") Departures
-    v-content
-      v-container(fluid)
-        v-fade-transition(mode="out-in")
-          router-view
+    v-layout(justify-center)
+      v-flex(xs12 sm10 md8)
+        v-toolbar(color="blue" dark fixed app)
+          v-toolbar-title(v-text="title")
+          //- v-spacer
+          //- v-toolbar-items
+            v-btn(flat to="/") HOME
+            v-btn(flat to="/departures") Departures
+        v-content
+          v-container(fluid)
+            v-fade-transition(mode="out-in")
+              router-view
 </template>
 
 
