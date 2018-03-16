57cac4b950db6c2d0dbb2501c80da9d5dd399a52
Michael Gecht <gecht.m@gmail.com>
2018-03-16 15:20:20 +0100

    Add action button to scroll back to top

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template lang="pug">
   v-app(light)
-    v-layout(justify-center)
-      v-flex(xs12 sm10 md8)
-        v-toolbar(color="blue" dark fixed app :scroll-off-screen="true")
-          v-toolbar-title(v-text="title")
-          //- v-spacer
-          //- v-toolbar-items
-            v-btn(flat to="/") HOME
-            v-btn(flat to="/departures") Departures
-        v-content
-          v-container(fluid)
+    v-toolbar(color="blue" dark fixed app :scroll-off-screen="true")
+      v-toolbar-title(v-text="title")
+      //- v-spacer
+      //- v-toolbar-items
+        v-btn(flat to="/") HOME
+        v-btn(flat to="/departures") Departures
+    v-content(v-scroll="onScroll")
+      v-container(fluid)
+        v-layout(justify-center)
+          v-flex(xs12 sm10 md8)
             v-fade-transition(mode="out-in")
               router-view
-    v-footer(class="pa-3") ASD
+    v-fab-transition
+      v-btn(
+        color="red"
+        dark
+        large
+        bottom
+        right
+        fab
+        fixed
+        v-show="offsetTop > 300"
+        @click="scrollToTop"
+        )
+        v-icon keyboard_arrow_up
 </template>
 
 
@@ -22,7 +34,16 @@ export default {
   data () {
     return {
       title: 'TAKE.ME.HOME.',
-      drawer: null
+      drawer: null,
+      offsetTop: 0
+    }
+  },
+  methods: {
+    scrollToTop () {
+      this.$vuetify.goTo('#app')
+    },
+    onScroll (e) {
+      this.offsetTop = window.pageYOffset || document.documentElement.scrollTop
     }
   },
   mounted () {
