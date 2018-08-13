5e1decf5ed5e3fa487ff51cb84688f149ceba8bf
Michael Gecht <gecht.m@gmail.com>
2018-08-13 17:32:02 +0200

    Update footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,27 +1,36 @@
 <template lang="pug">
-  v-footer(height="auto")
+  v-footer(dark height="auto")
     v-card(
+      class="flex"
       flat
       tile
-      width="100%"
-      class="blue white--text text-xs-center"
       )
-      v-card-text
-        strong Initiative to get rid of ugly user interfaces. Looking at you RMV!
-      v-card-text
-        strong Build in Frankfurt. Hosted on <a href="https://zeit.co/now">now.sh</a>.
-      v-card-text
-        strong Code available on GitHub!
-        v-btn(
-          icon
-          class="mx-3 white--text"
-          href="https://github.com/mimischi/takemehome"
-          )
-          v-icon fab fa-github
+      v-layout(:class="color")
+        v-card-title
+          strong(class="subheading") Initiative to get rid of ugly user interfaces. Looking at you RMV.de! Code is open source and to be found on GitHub.
+          v-btn(
+            icon
+            class="mx-3 white--text"
+            href="https://github.com/mimischi/takemehome"
+            )
+            v-icon(size="24px") fab fa-github
+      v-card-actions(:class="copyClass") &copy; {{ yearNotice }} - Built in Frankfurt, brudi!
 </template>
 
 <script>
 export default {
-  name: 'TheFooter'
+  name: 'TheFooter',
+  data: () => ({
+    color: 'blue'
+  }),
+  computed: {
+    copyClass: function () {
+      return `${this.color} darken-3 justify-center`
+    },
+    yearNotice: function () {
+      const thisYear = new Date().getFullYear()
+      return `2017-${thisYear}`
+    }
+  }
 }
 </script>
