698addd64f6d1dc5e5be0e42de328e35e8567e67
Michael Gecht <gecht.m@gmail.com>
2018-08-13 23:49:01 +0200

    Use HTML5 history mode in VueRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Connection from '@/components/connection/Connection'
-// import Departures from '@/components/departures/Departures'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Connection',
       component: Connection
+    },
+    {
+      path: '*',
+      component: Connection
     }
-    // {
-    //   path: '/departures',
-    //   name: 'Departures',
-    //   component: Departures
-    // }
   ]
 })
