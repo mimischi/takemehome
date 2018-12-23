4571a735775a3da743c7e29879d9a806993472be
Michael Gecht <gecht.m@gmail.com>
2018-12-23 11:57:10 +0100

    Remove MDI and add Material Icons

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify, {
   iconfont: "mdi"
