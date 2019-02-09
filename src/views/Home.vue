3d82e46bf39a06604d694cd50eb35f94e2d3c782
Michael Gecht <gecht.m@gmail.com>
2019-02-09 09:59:39 +0100

    Redirect to user-defined start page from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-connection-timeline
+p(v-if="staying") HELLO
 </template>
 
 <script>
@@ -8,6 +8,30 @@ import ConnectionTimeline from "@/components/ConnectionTimeline";
 export default {
   components: {
     ConnectionTimeline
+  },
+  data: () => ({
+    staying: false
+  }),
+  computed: {
+    connection() {
+      return this.$store.state.connections[0] || null;
+    }
+  },
+  mounted() {
+    if (this.$store.state.settings.homepage === null) {
+      this.staying = true;
+      return;
+    }
+
+    let router = {};
+    if (this.$store.state.settings.homepage === "cards") {
+      router["name"] = "cards";
+    } else {
+      router["name"] = "lookup";
+      router["params"] = { id: this.$store.state.connections[0].uuid };
+    }
+
+    this.$router.push(router);
   }
 };
 </script>
