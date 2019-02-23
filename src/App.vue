f7d41d1adabd06f39590bf2cf6174171869ba1dd
Michael Gecht <gecht.m@gmail.com>
2019-02-23 12:08:44 +0100

    Update App layout and add TheCard.
    Resolves #9.

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,58 @@
 <template lang="pug">
 v-app
-  v-toolbar(app)
-    v-toolbar-title(class="headline text-uppercase")
-      v-btn(
-        color="primary"
-        flat
-        :ripple="{ class: 'primary--text' }"
-        active-class=""
-        to="/"
-      )
-        span T.
-        span(class="font-weight-light") M.
-        span H.
-    v-spacer
-    v-btn(flat :to="{ name: 'settings' }") Settings
-    v-btn(flat :to="{ name: 'connectionList' }") Connections
+  v-navigation-drawer(
+    v-model="drawer"
+    app
+    clipped
+    mini-variant
+    permanent
+  )
+    v-list(dense)
+      v-list-tile(:to="{ name: 'connectionList' }" exact)
+        v-list-tile-action
+          v-icon commute
+        v-list-tile-content
+          v-list-tile-title Connections
+      v-list-tile(:to="{ name: 'settings' }" exact)
+        v-list-tile-action
+          v-icon settings
+        v-list-tile-content
+          v-list-tile-title Settings
+  v-toolbar(
+    app
+    dark
+    extended
+    clipped-left
+    color="primary"
+    flat
+  )
+    //- v-toolbar-title(class="headline text-uppercase")
+    //-   v-btn(
+    //-     color="white"
+    //-     flat
+    //-     :ripple="{ class: 'white--text' }"
+    //-     active-class=""
+    //-     to="/"
+    //-     small
+    //-   )
+    //-     span T.
+    //-     span(class="font-weight-light") M.
+    //-     span H.
 
   v-content
     v-container(fluid)
       v-layout(justify-center)
         v-flex(xs12)
-          v-slide-y-transition(hide-on-leave)
-            router-view
+          router-view
 
   portal-target(name="fab")
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data: () => ({
+    drawer: false
+  })
 };
 </script>
---
+++ src/components/TheCard.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+v-card(class="card--flex-toolbar")
+  v-toolbar(card prominent)
+    v-toolbar-title(class="body-2 grey--text")
+      template(v-if="backLink")
+        v-btn(
+          icon
+          :to="backLink"
+          exact
+        )
+          v-icon chevron_left
+      template(v-else) {{ title }}
+    v-spacer
+    slot(name="rightSide")
+  v-divider
+
+  slot
+    span Nothing to show here.
+</template>
+
+<script>
+export default {
+  name: "TheCard",
+  props: {
+    title: {
+      type: String
+    },
+    backLink: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style>
+.card--flex-toolbar {
+  margin-top: -80px;
+  z-index: 4;
+}
+</style>
