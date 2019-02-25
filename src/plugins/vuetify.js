3ae18522d01bbc4c52fc5b6071d48c36652c5e32
Michael Gecht <gecht.m@gmail.com>
2019-02-25 14:40:45 +0100

    Fix icons typo

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,5 +5,5 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify, {
-  iconfont: "mdi"
+  iconfont: "md"
 });
